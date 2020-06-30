97aa4373389ded547993c3468517a126f3c7d48a
krtya.admin <krtya.admin@krtyasoftwares.net>
2020-06-30 15:32:14 +0530

    update

--- src/Components/ForgetPassword.js
+++ src/Components/ForgetPassword.js
@@ -4,93 +4,90 @@ import Footer from '../Footer';
 //import FormValidator from './FromValidator';
 
 class ForgetPassword extends Component {
-    constructor() {
-        super();
-        this.state = {
-            fields: {},
-            errors: {}
-          }
-          this.handleChange = this.handleChange.bind(this);
-          this.submitsignupForm = this.submitsignupForm.bind(this);
-    
-        };
-    
-        handleChange(e) {
-          let fields = this.state.fields;
-          fields[e.target.name] = e.target.value;
-          this.setState({
-            fields
-          });
-    
-        }
-    
-        submitsignupForm(e) {
-          e.preventDefault();
-          if (this.validateForm()) {
-              let fields = {};
-             fields["email"] = "";
-             
-            
-              this.setState({fields:fields});
-              alert("Form has been submitted");
-          }
-    
-        }
-    
-        validateForm() {
-    
-          let fields = this.state.fields;
-          let errors = {};
-          let formIsValid = true;
-    
-          if (!fields["email"]) {
-            formIsValid = false;
-            errors["email"] = "*Please enter your email.";
-          }
-    
-          if (typeof fields["email"] !== "undefined") {
-            //regular expression for email validation
-            var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
-            if (!pattern.test(fields["email"])) {
-              formIsValid = false;
-              errors["email"] = "*Please enter valid email.";
-            }
-          }
-  
+  constructor() {
+    super();
+    this.state = {
+      fields: {},
+      errors: {}
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.submitsignupForm = this.submitsignupForm.bind(this);
+
+  };
+
+  handleChange(e) {
+    let fields = this.state.fields;
+    fields[e.target.name] = e.target.value;
+    this.setState({
+      fields
+    });
+
+  }
+
+  submitsignupForm(e) {
+    e.preventDefault();
+    if (this.validateForm()) {
+      let fields = {};
+      fields["email"] = "";
+      this.setState({ fields: fields });
+      alert("Form has been submitted");
     }
 
-    componentDidMount() {
-        document.title = "Igyanam - Forgot Password";
-        window.scrollTo(0, 0);
+  }
+
+  validateForm() {
+    let fields = this.state.fields;
+    let errors = {};
+    let formIsValid = true;
+
+    if (!fields["email"]) {
+      formIsValid = false;
+      errors["email"] = "*Please enter your email.";
     }
 
-    render() {
-        return (
-            <React.Fragment>
-                <Header />
-              
-                    <main className="flex-shrink-0">
-                        <section className="common-block">
-                            <div className="container">
-                                <div className="login-main">
-                                    <h2 className="mb-3"> Forgot Password</h2>
-                                    <form method="post"  name="ForgotPassword"  onSubmit= {this.submitsignupForm} >
-                                    <div className="white-box-no-animate p-20">
-                                        <div className="form-group">
-                                            <label htmlFor="exampleInputEmail1">Email </label>
-                                            <input type="email"  placeholder="Enter The Email" name='email' value={this.state.fields.email} onChange={this.handleChange} className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-                                        </div>
-                                        <button type="submit" className="btn btn-primary" value="Next">Next</button>
-                                    </div>
-                                    </form>
-                                </div>
-                            </div>
-                        </section>
-                    </main>
-                <Footer />
-            </React.Fragment>
-        );
+    if (typeof fields["email"] !== "undefined") {
+      //regular expression for email validation
+      var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
+      if (!pattern.test(fields["email"])) {
+        formIsValid = false;
+        errors["email"] = "*Please enter valid email.";
+      }
     }
+
+  }
+
+  componentDidMount() {
+    document.title = "Igyanam - Forgot Password";
+    window.scrollTo(0, 0);
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <Header />
+
+        <main className="flex-shrink-0">
+          <section className="common-block">
+            <div className="container">
+              <div className="login-main">
+                <h2 className="mb-3"> Forgot Password</h2>
+                <form method="post" name="ForgotPassword" onSubmit={this.submitsignupForm} >
+                  <div className="white-box-no-animate p-20">
+                    <div className="form-group">
+                      <label htmlFor="exampleInputEmail1">Email </label>
+                      <input type="email" placeholder="Enter The Email" name='email' value={this.state.fields.email} onChange={this.handleChange} className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                    </div>
+                    <button type="submit" className="btn btn-primary" value="Next">Next</button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </section>
+        </main>
+        <Footer />
+      </React.Fragment>
+    );
+  }
 }
 
 export default ForgetPassword;
--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -3,135 +3,110 @@ import Header from '../Header';
 import Footer from '../Footer';
 import FormValidator from './FromValidator';
 
-
-
-
-
 class Signup extends Component {
 
-    constructor() {
-        super();
+  constructor() {
+    super();
 
-        this.validator = new FormValidator([
-          { 
-            field: 'fullName', 
-            method: 'isEmpty', 
-            validWhen: false, 
-            message: 'Enter full name.' 
-          },
-          { 
-              field: 'email', 
-              method: 'isEmpty', 
-              validWhen: false, 
-              message: 'Enter your email .' 
-            },
-            { 
-              field: 'email',
-              method: 'isEmail', 
-              validWhen: true, 
-              message: 'Enter valid email.'
-            },
-            { 
-              field: 'mobile', 
-              method: 'isEmpty', 
-              validWhen: false, 
-              message: 'Enter a phone number.'
-            },
-            {
-              field: 'mobile', 
-              method: 'matches',
-              args: [/^\(?\d\d\d\)? ?\d\d\d-?\d\d\d\d$/],
-              validWhen: true, 
-              message: 'Enter valid phone number.'
-            },
-      ]);
+    this.validator = new FormValidator([
+      {
+        field: 'fullName',
+        method: 'isEmpty',
+        validWhen: false,
+        message: 'Enter full name.'
+      },
+      {
+        field: 'email',
+        method: 'isEmpty',
+        validWhen: false,
+        message: 'Enter your email .'
+      },
+      {
+        field: 'email',
+        method: 'isEmail',
+        validWhen: true,
+        message: 'Enter valid email.'
+      },
+      {
+        field: 'mobile',
+        method: 'isEmpty',
+        validWhen: false,
+        message: 'Enter Mobile No.'
+      },
+      {
+        field: 'mobile',
+        method: 'matches',
+        args: [/^\(?\d\d\d\)? ?\d\d\d-?\d\d\d\d$/],
+        validWhen: true,
+        message: 'Enter valid Mobile No.'
+      },
+    ]);
 
-      this.state = {
-          fullName : '',
-          email: '',
-          mobile: '',
-          validation: this.validator.valid(),
-
-        }
-        this.submitted = false;
-      }
-      handleInputChange = event => {
-          event.preventDefault();
-      
-          this.setState({
-            [event.target.name]: event.target.value,
-          });
-        }
-          
-        handleFormSubmit = event => {
-          event.preventDefault();
-      
-          const validation = this.validator.validate(this.state);
-          this.setState({ validation });
-          this.submitted = true;
-      
-          if (validation.isValid) {
-            //reaches here if form validates successfully...
-          }
-        }
-       
-          
-        
-
-    
-    
-    componentDidMount() {
-        document.title = "Igyanam - Sign Up";
-        window.scrollTo(0, 0);
+    this.state = {
+      fullName: '',
+      email: '',
+      mobile: '',
+      validation: this.validator.valid(),
     }
-    render() {
-      const validation = this.submitted ?this.validator.validate(this.state) : this.state.validation    
-        return (
-            <React.Fragment>
-                <Header />
-                <main className="flex-shrink-0">
-                    <section className="common-block">
-                        <div className="container">
-                            <div className="login-main">
-                                <h2 className="mb-3"> Student Sign Up</h2>
-                                {/* <form method="post"  name="userSignUpForm"  onSubmit= {this.submitsignupForm} > */}
- 
-                                <div className="white-box-no-animate p-20">
-                                    <div className="form-group">
-                                    <div className={validation.email.isInvalid && 'has-error'}>
-                                        <label >Full Name</label>
-                                        <input type="string" placeholder="Enter The Full Name" className="form-control"  onChange={this.handleInputChange} name='fullName' />
-                                        {/* <div className="errorMsg">{this.state.errors.fullName}</div> */}
-                                        <span className="help-block">{validation.fullName.message}</span>
-                                    </div>
-                                    </div>
-                                    <div className="form-group">
-                                    <div className={validation.email.isInvalid && 'has-error'}>
-                                        <label htmlFor="exampleInputEmail1">Email</label>
-                                        <input type="email" placeholder="Enter The Email" className="form-control"  name='email' onChange={this.handleInputChange} />
-                                        {/* <div className="errorMsg">{this.state.errors.email}</div> */}
-                                        <span className="help-block">{validation.email.message}</span>
-                                      </div>
-                                    </div>
-                                    <div className="form-group">
-                                    <div className={validation.mobile.isInvalid && 'has-error'}>
-                                        <label >Mobile</label>
-                                        <input type="mobile" placeholder="Enter The Mobile No" className="form-control" name='mobile' onChange={this.handleInputChange} />
-                                        {/* <div className="errorMsg">{this.state.errors.mobile}</div> */}
-                                        <span className="help-block">{validation.mobile.message}</span>
-                                      </div>
-                                    </div>
-                                    <button type="submit" onClick={this.handleFormSubmit} className="btn btn-primary">Submit</button>
-                                </div>
-                                {/* </form> */}
-                            </div>
-                        </div>
-                    </section>
-                </main>
-                <Footer />
-            </React.Fragment>
-        );
-    }
+    this.submitted = false;
+  }
+
+  handleInputChange = event => {
+    event.preventDefault();
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  }
+
+  handleFormSubmit = event => {
+    event.preventDefault();
+    const validation = this.validator.validate(this.state);
+    this.setState({ validation });
+    this.submitted = true;
+  }
+
+  componentDidMount() {
+    document.title = "Igyanam - Sign Up";
+    window.scrollTo(0, 0);
+  }
+  render() {
+    const validation = this.submitted ? this.validator.validate(this.state) : this.state.validation
+    return (
+      <React.Fragment>
+        <Header />
+        <main className="flex-shrink-0">
+          <section className="common-block">
+            <div className="container">
+              <div className="login-main">
+                <h2 className="mb-3"> Student Sign Up</h2>
+                <form method="post" name="userSignUpForm" onChange={this.handleInputChange} >
+                  <div className="white-box-no-animate p-20">
+                    <div className="form-group">
+                      <label >Full Name <span style={{ color: 'red' }}>*</span></label>
+                      <input type="string" placeholder="Enter The Full Name" className="form-control" name='fullName' />
+                      <span className="help-block">{validation.fullName.message}</span>
+                    </div>
+                    <div className="form-group">
+                      <label htmlFor="exampleInputEmail1">Email <span style={{ color: 'red' }}>*</span></label>
+                      <input type="email" placeholder="Enter The Email" className="form-control" name='email' />
+                      <span className="help-block">{validation.email.message}</span>
+                    </div>
+                    <div className="form-group">
+                      <label >Mobile <span style={{ color: 'red' }}>*</span></label>
+                      <input type="mobile" placeholder="Enter The Mobile No" className="form-control" name='mobile' />
+                      <span className="help-block">{validation.mobile.message}</span>
+                    </div>
+                    <button type="submit" onClick={this.handleFormSubmit} className="btn btn-primary">Submit</button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </section>
+        </main>
+        <Footer />
+      </React.Fragment>
+    );
+  }
 }
 
 export default Signup;
