3e87d21c257ff617a83f9a6fc73e868d5fd8f926
krtya.admin <krtya.admin@krtyasoftwares.net>
2020-07-11 16:44:48 +0530

    signup.js update

--- src/Components/Pages/Signup.js
+++ src/Components/Pages/Signup.js
@@ -6,9 +6,9 @@ import BsSignUp from '../../Core/Services/SignUp/BsSignUp'
 import swal from 'sweetalert';
 
 class Signup extends Component {
-  _isMounted = false;
-  constructor(props) {
-    super(props);
+
+  constructor() {
+    super();
 
     this.validator = new FormValidator([
       {
@@ -45,10 +45,12 @@ class Signup extends Component {
     ]);
 
     this.state = {
-      fullName: '',
+      fullname: '',
       email: '',
+      mobile: '',
       mobile_number: '',
       responseData: '',
+      loading: false,
       validation: this.validator.valid(),
     }
     this.submitted = false;
@@ -65,24 +67,23 @@ class Signup extends Component {
     event.preventDefault();
     const validation = this.validator.validate(this.state);
     this.setState({ validation });
-
-    let myForm = document.getElementById('myForm');
-    let formData = new FormData(myForm);
-    var object = {};
-    formData.forEach((value, key) => { object[key] = value });
-    var json = '{"property":' + JSON.stringify(object) + '}';
-
-    console.log(json);
-
-    BsSignUp.signUp(json)
-    console.log('done');
-    swal({
-      title: "Sign Up Success!",
-      text: "You clicked the button!",
-      icon: "success",
-    });
-    this.props.history.push('/signin')
-    this.submitted = true;
+    if (validation.isValid) {
+      let myForm = document.getElementById('myForm');
+      let formData = new FormData(myForm);
+      var object = {};
+      formData.forEach((value, key) => { object[key] = value });
+      var json = '{"property":' + JSON.stringify(object) + '}';
+      console.log(json);
+      BsSignUp.signUp(json)
+      console.log('done');
+      this.setState({ submitted: true });
+      this.setState({ loading: false })
+      swal({
+        title: "Sign Up Success!",
+        icon: "success",
+      });
+      this.props.history.push('/signin')
+    }
   }
 
   componentDidMount() {
@@ -92,6 +93,7 @@ class Signup extends Component {
 
   render() {
     const validation = this.submitted ? this.validator.validate(this.state) : this.state.validation
+    const { loading, error } = this.state;
     return (
       <React.Fragment>
         <Header />
@@ -101,23 +103,27 @@ class Signup extends Component {
               <div className="login-main">
                 <h2 className="mb-3"> Student Sign Up</h2>
                 <form method="post" id="myForm" name="userSignUpForm" onChange={this.handleInputChange} >
+                  {error && <div className="alert alert-danger">{error}</div>}
                   <div className="white-box-no-animate p-20">
                     <div className="form-group">
                       <label >Full Name <span style={{ color: 'red' }}>*</span></label>
-                      <input type="string" placeholder="Enter The Full Name" className="form-control" name='fullname' />
+                      <input type="string" placeholder="Enter The Full Name" className="form-control" name='fullname' onChange={this.handleInputChange} />
                       <span className="help-block">{validation.fullname.message}</span>
                     </div>
                     <div className="form-group">
                       <label htmlFor="exampleInputEmail1">Email <span style={{ color: 'red' }}>*</span></label>
-                      <input type="email" placeholder="Enter The Email" className="form-control" name='email' />
+                      <input type="email" placeholder="Enter The Email" className="form-control" aria-describedby="emailHelp" name='email' onChange={this.handleInputChange} />
                       <span className="help-block">{validation.email.message}</span>
                     </div>
                     <div className="form-group">
                       <label >Mobile <span style={{ color: 'red' }}>*</span></label>
-                      <input type="mobile" placeholder="Enter The Mobile No" className="form-control" name='mobile_number' />
+                      <input type="mobile" placeholder="Enter The Mobile No" className="form-control" name='mobile_number' onChange={this.handleInputChange} />
                       <span className="help-block">{validation.mobile_number.message}</span>
                     </div>
-                    <button type="submit" onClick={this.handleFormSubmit} className="btn btn-primary">Submit</button>
+                    <button type="submit" onClick={this.handleFormSubmit} className="btn btn-primary" disabled={loading}>
+                      {loading && <span className="spinner-border spinner-border-sm mr-1"></span>}
+                        Submit
+                    </button>
                   </div>
                 </form>
               </div>
