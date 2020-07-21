10ad45428ebc8ed1914c3015f46957ee69457e07
jariwbh <jariwbh@krtya.com>
2020-07-21 11:36:50 +0530

    update contact.js

--- src/Components/Pages/Contactus.js
+++ src/Components/Pages/Contactus.js
@@ -1,19 +1,29 @@
 import React, { Component } from 'react';
 import Header from './Header';
 import Footer from './Footer';
+import DemoService from '../../Core/Services/DemoService/DemoServices';
+
 
 const MapURL = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d782.0102619821901!2d72.78420635307717!3d21.196581104939156!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x9cf1ece5e9fad379!2sUrjaa%20Fitness%20%3A%20GYM!5e0!3m2!1sen!2sin!4v1592819663012!5m2!1sen!2sin";
 class Contactus extends Component {
     constructor() {
         super();
+        this.state = {
+            details: []
+        }
     }
 
     componentDidMount() {
         document.title = "Igyanam - Contact Us";
         window.scrollTo(0, 0);
+        DemoService.getClientDetails()
+            .then(data => {
+                this.setState({ details: data })
+            })
     }
 
     render() {
+        const { details } = this.state;
         return (
             <React.Fragment>
                 <Header />
@@ -25,19 +35,18 @@ class Contactus extends Component {
                                 <div className="row">
                                     <div className="col-lg-4 col-md-6 xs-mrb30 md-mrb30">
                                         <div className="tp-head-sm mb-1">Address</div>
-                                        iGYANAM<br />
-                                        Riddhi Residency, 1st floor,<br />
-                                            Above Kotak Mahindra Bank,<br />
-                                                Adajan, Surat, Guajarat.
-			                        </div>
+                                        {details.branchname}<br />
+                                        {details.address},<br />
+                                        {details.city} {details.postcode}
+                                    </div>
                                     <div className="col-lg-4 col-md-6 xs-mrb30 md-mrb30">
                                         <div className="tp-head-sm mb-1">Phone</div>
-                                            9099002757 / 9694418782
+                                        {details.companyphone}
                                     </div>
                                     <div className="col-lg-4 col-md-6 xs-mrb30">
                                         <div className="tp-head-sm mb-1">Email</div>
-                                            igyanammocktest@gmail.com
-                                </div>
+                                        {details.supportemail}
+                                    </div>
                                 </div>
                             </div>
                             <div className="white-box-no-animate p-20">
